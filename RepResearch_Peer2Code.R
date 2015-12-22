e11bab1ec74385cd340763ca84d955697287ba7e
YspAnimal <Animal1986@mail.ru>
2015-12-22 12:09:06 +0400

    st

---
+++ RepResearch_Peer2Code.R
@@ -0,0 +1,12 @@
+#Set working directory and then download file, unzip it if it does not exist
+setwd("C:/Users/soloveynv/Documents/R Scripts/Coursera/RepResearch_Peer2")
+library(data.table)
+library(R.utils)
+URL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+destFile <- "StormData.csv.bz2"
+DataFile <- "StormData.csv"
+if (!file.exists(destFile)){
+  download.file(URL, destfile = destFile, mode='wb')
+}
+bunzip2(destFile, DataFile)
+StormData <- fread(DataFile, nrows = 902297)
