fafbc87350f97d4ab31a849e47eee78294a7c149
YspAnimal <Animal1986@mail.ru>
2015-12-23 15:46:28 +0400

    2

--- RepResearch_Peer2Code.R
+++ RepResearch_Peer2Code.R
@@ -2,6 +2,9 @@
 setwd("C:/Users/soloveynv/Documents/R Scripts/Coursera/RepResearch2")
 library(data.table)
 library(R.utils)
+library(ggplot2)
+library(plyr)
+library(dplyr)
 URL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 destFile <- "StormData.csv.bz2"
 DataFile <- "StormData.csv"
@@ -10,4 +13,11 @@ if (!file.exists(destFile)){
 }
 bunzip2(destFile, DataFile)
 StormData <- fread(DataFile)
+str(StormData)
+StormData$BGN_DATE <- as.Date(StormData$BGN_DATE, format = "%m/%d/%Y %H:%M:%S")
+
+#Aggregate data frame by EVType and sum of fatalities and injuries
+
+StormDataAGG <- ddply(StormData, "EVTYPE", function(X) data.frame(FATALITIES=sum(X$FATALITIES),INJURIES=sum(X$INJURIES)))
+StormDataAGG <- filter(StormDataAGG, FATALITIES>0 | INJURIES>0)
 
